25e367d9d7a9dda063892ec753d50bc33df6d96e
moyan <rugang6891@163.com>
2011-06-09 01:53:06 +0000

    by drg

--- static/js/common/init.js
+++ static/js/common/init.js
@@ -6,3 +6,20 @@ seajs.config({
     'rm_util': 'js/common/rm_util.js'
   }
 });
+
+
+// index module
+define(function(require, exports, module) {
+  var $ = require('jquery');
+  var rm_util = require('rm_util');
+  
+  
+  $(document).ready(function() {
+    // the process after the page loaded
+    // alert("hello world hello");
+    // add tip effect on email
+    rm_util.addInputTipEffect($("#email"), $("#efs"));
+    rm_util.addInputTipEffect($("#password"), $("#pfs"));
+  });
+  
+});
